dc2117d3bab018bda49b6ab7c4405a24b46b0102
minrong <cocomong9997@naver.com>
2021-11-01 03:37:15 +0900

    community CRUD 완성

--- community/views.py
+++ community/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Community
+from django.utils import timezone
 # Create your views here.
 
 
@@ -11,3 +12,35 @@ def communityBoard(request):
 def communityDetail(request, community_id):
     community_detail = get_object_or_404(Community, pk=community_id)
     return render(request, 'community/communityDetail.html', {'community': community_detail})
+
+
+def communityNew(request):
+    return render(request, 'community/communityNew.html')
+
+
+def communityCreate(request):
+    new_community = Community()
+    new_community.title = request.POST['title']
+    new_community.body = request.POST['body']
+    new_community.date = timezone.now()
+    new_community.save()
+    return redirect('community:communityBoard')
+
+
+def communityEdit(request, community_id):
+    community_detail = get_object_or_404(Community, pk=community_id)
+    return render(request, 'community/communityEdit.html', {'community': community_detail})
+
+
+def communityUpdate(request, community_id):
+    community_update = get_object_or_404(Community, pk=community_id)
+    community_update.title = request.POST['title']
+    community_update.body = request.POST['body']
+    community_update.save()
+    return redirect('community:communityBoard')
+
+
+def communityDelete(request, community_id):
+    community_delete = get_object_or_404(Community, pk=community_id)
+    community_delete.delete()
+    return redirect('community:communityBoard')
