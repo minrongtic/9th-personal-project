177507e9e1284b6cdd8fe9851cc9374d68f3a831
minrong <cocomong9997@naver.com>
2021-11-14 18:13:43 +0900

    route Board 부트스트랩 완

--- Esainbolt/route/tests.py
+++ Esainbolt/route/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+ <!-- < div class = "fin" >
+        <h2 > 도착지: {{route.fin}} < /h2 >
+    </div>
+    <div class="via">
+        <p>가는 방법: {{route.via}}</p>
+    </div>
+    <div class="more">
+        <a href="{% url 'route:routeDetail' route.id %}">more</a>
+    </div>
+    <br><br>
+    {% endfor %}
+{% endblock body-content %} -->
