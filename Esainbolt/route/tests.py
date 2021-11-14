59cedf34eda26a6ee88de289bcde631fef734849
minrong <cocomong9997@naver.com>
2021-11-14 18:50:48 +0900

    route detail&board 부트스트랩 완

--- Esainbolt/route/tests.py
+++ Esainbolt/route/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+# route Board 원래
  <!-- < div class = "fin" >
         <h2 > 도착지: {{route.fin}} < /h2 >
     </div>
@@ -13,3 +14,16 @@ from django.test import TestCase
     <br><br>
     {% endfor %}
 {% endblock body-content %} -->
+
+# route detail 원래
+<div class="fin">
+        <h2>도착지: {{route.fin}}</h2>
+    </div>
+    <div class="via">
+        <p>가는 방법: {{route.via}}</p>
+    </div>
+    <div class="etc">
+        <p>비고: {{route.etc|linebreaks}}</p>
+    </div>
+
+<a href="{% url 'route:routeDetail' route.id %}" class="stretched-link">자세히 보기</a>
